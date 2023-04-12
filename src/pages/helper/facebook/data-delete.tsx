6d12c36f1fd73a6b38de424b5e705085bc2c5131
FabricioChiaradia <fabricioxc@gmail.com>
2023-04-12 19:41:04 -0300

    feat: update facebook delete data

---
+++ src/pages/helper/facebook/data-delete.tsx
@@ -0,0 +1,39 @@
+const FacebookDataDelete = () => {
+  return (
+    <div
+      style={{
+        padding: "5%",
+        maxWidth: "900px",
+        display: "flex",
+        flexDirection: "column",
+        // alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p>
+        TCC PUC 2023 é uma aplicação que permite o login no Facebook e não
+        guardamos os seus dados pessoais no nosso servidor. De acordo com a
+        política do Facebook, devemos fornecer o URL do lembrete de exclusão de
+        dados do usuário ou o URL da instrução de exclusão de dados.
+      </p>
+      <br></br>
+      <p>
+        Se deseja apagar as suas actividades para a aplicação TCC PUC 2023, pode
+        apagar as suas informações seguindo estes passos:
+      </p>
+      <br></br>
+      <p>
+        Vá para as configurações da sua conta do Facebook e privacidade. Clique
+        em Configurações.
+      </p>
+      <p>
+        Procure por Aplicativos e sites e você verá todos os aplicativos e sites
+        que você vinculou ao seu Facebook.
+      </p>
+      <p>Procure e clique em TCC PUC 2023 na barra de pesquisa.</p>
+      <p>Role para baixo e clique em Delete.</p>
+      <p>Parabéns, você removeu com sucesso as atividades da sua inscrição.</p>
+    </div>
+  );
+};
+export default FacebookDataDelete;
