b769c8c650e4119499597dad3896f2834c999ed3
FabricioChiaradia <fabricioxc@gmail.com>
2023-04-30 09:36:07 -0300

    feat: update code

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,12 +4,16 @@ import config from "./config/config.mjs";
 let sequelize;
 if (process.env.NODE_ENV === "production") {
   sequelize = new Sequelize(config.production);
+  console.log("Production mode");
 } else if (process.env.NODE_ENV === "staging") {
   sequelize = new Sequelize(config.staging);
+  console.log("Staging mode");
 } else if (process.env.NODE_ENV === "test") {
+  console.log("Test mode");
   sequelize = new Sequelize(config.test);
 } else {
   sequelize = new Sequelize(config.development);
+  console.log("Development mode");
 }
 
 const connection = sequelize;
