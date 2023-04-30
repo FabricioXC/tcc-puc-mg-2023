3ee9133fa842bb1f63513e3c0aaf0229933dfcc4
FabricioChiaradia <fabricioxc@gmail.com>
2023-04-30 12:24:47 -0300

    update

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,33 +2,19 @@ import Sequelize from "sequelize";
 import config from "./config/config.mjs";
 
 let sequelize;
-// if (process.env.NODE_ENV === "production") {
-//   sequelize = new Sequelize(config.production);
-//   console.log("Production mode");
-// } else if (process.env.NODE_ENV === "staging") {
-//   sequelize = new Sequelize(config.staging);
-//   console.log("Staging mode");
-// } else if (process.env.NODE_ENV === "test") {
-//   console.log("Test mode");
-//   sequelize = new Sequelize(config.test);
-// } else {
-//   sequelize = new Sequelize(config.development);
-//   console.log("Development mode");
-// }
-const DB_USERNAME = "sql9614938";
-const DB_PASSWORD = "waxNAcz2tV";
-const DB_NAME = "sql9614938";
-const DB_HOSTNAME = "sql9.freemysqlhosting.net";
-const DB_PORT = "3306";
-sequelize = new Sequelize({
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  host: DB_HOSTNAME,
-  port: DB_PORT,
-  logging: false,
-  dialect: "mysql",
-});
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(config.production);
+  console.log("Production mode");
+} else if (process.env.NODE_ENV === "staging") {
+  sequelize = new Sequelize(config.staging);
+  console.log("Staging mode");
+} else if (process.env.NODE_ENV === "test") {
+  console.log("Test mode");
+  sequelize = new Sequelize(config.test);
+} else {
+  sequelize = new Sequelize(config.development);
+  console.log("Development mode");
+}
 
 const connection = sequelize;
 
