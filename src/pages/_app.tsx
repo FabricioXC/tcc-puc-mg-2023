2d25a1f564a56d3d9a46d3c20199c21d1302d5a0
FabricioChiaradia <fabricioxc@gmail.com>
2023-04-30 09:49:48 -0300

    feat: update code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,18 @@ import type { AppProps } from "next/app";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
-import { SessionProvider } from "next-auth/react";
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+// import { SessionProvider } from "next-auth/react";
+// export default function App({
+//   Component,
+//   pageProps: { session, ...pageProps },
+// }: AppProps) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// }
